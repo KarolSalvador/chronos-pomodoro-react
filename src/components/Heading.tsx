323921e108a66e713494dd4407a1a863570dd625
KarolSalvador <karolsalvador91@gmail.com>
2025-05-20 19:00:40 -0300

    criando arquivos de estilos globais

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,6 @@
+import styles from './Heading.module.css';
+
 export function Heading() {
-    return <h1 className='heading'>Olá mundo!</h1>
+   //no JSX usa-se className para nomear classes de HTML afim de evitar conflito com a Classe de JS
+   return <h1 className={styles.heading}>Olá mundo!</h1>;
 }
