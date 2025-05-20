fccc2b9efa9a6d0ac30d6cf0da47c0d6122b91be
KarolSalvador <karolsalvador91@gmail.com>
2025-05-20 18:16:55 -0300

    criando component heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
     return (
         <>
-            <h1>Olá mundo!</h1>
+            <Heading />
+           
         </>
     )
 }
