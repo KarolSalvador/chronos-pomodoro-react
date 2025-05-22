bd8060c6283ee026d2301250a6881ca29c971b5e
KarolSalvador <karolsalvador91@gmail.com>
2025-05-22 15:15:07 -0300

    Finalizacao do componente heading e tupagem TypeScript de children

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,11 @@
 import styles from './Heading.module.css';
 
-export function Heading(props: any) {
+type HeadingProps = {
+  children: React.ReactNode; //esse tipo define que tudo que o react aceita como children estará contemplado nele
+};
+
+export function Heading({ children }: HeadingProps) {
   //no JSX usa-se className para nomear classes de HTML afim de evitar conflito com a Classe de JS
-  return <h1 className={styles.props}>{props.children}</h1>;
+
+  return <h1 className={styles.heading}>{children}</h1>;
 }
