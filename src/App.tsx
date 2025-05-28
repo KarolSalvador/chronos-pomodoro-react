44cb0e60f564e3220f510f9bfa35a5b38d96179f
KarolSalvador <karolsalvador91@gmail.com>
2025-05-28 16:50:51 -0300

    Criando a pagina Home e templante para todas as outras paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { DefaultButton } from './components/DefaultButton';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { PlayCircleIcon } from 'lucide-react';
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Footer } from './components/Footer';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              id='MeuInput'
-              type='text'
-              placeholder='Ex.: estudar para a prova'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate/Index';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
+          quia, eveniet minus quaerat adipisci quae accusantium beatae
+          architecto repellat eaque suscipit expedita maiores ratione totam quis
+          perspiciatis distinctio ipsum modi.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
