e448d1a1a7a709b7638fc7f70becb5cccda6b530
KarolSalvador <karolsalvador91@gmail.com>
2025-05-28 16:29:23 -0300

    salvando tema na localStorage e alterando icone de tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,10 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
@@ -6,7 +12,17 @@ import { useState, useEffect } from 'react';
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -22,7 +38,8 @@ export function Menu() {
   //permite executar efeitos colaterais nos componentes
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
-  }, [theme]); //essa forma passando a função e uma dependência executa apenas quando o valor do theme muda
+    localStorage.setItem('theme', theme);
+  }, [theme]); //essa forma passando a função e uma dependência executa apenas quando o valor do theme muda (ocorre quando há um click no ícone sun)
 
   return (
     <nav className={styles.menu}>
@@ -60,7 +77,7 @@ export function Menu() {
         title='Mudar Tema'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
