e41d13f39a0eb81ddad6bd25a116c50c9a4585ae
KarolSalvador <karolsalvador91@gmail.com>
2025-06-10 10:53:53 -0300

    Context API, useContext + useState (estado+contexto)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,9 +2,8 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import type { HomeProps } from '../../pages/Home';
 
-export function MainForm({ state }: HomeProps) {
+export function MainForm() {
   return (
     <form className='form' action=''>
       <div className='formRow'>
@@ -17,7 +16,7 @@ export function MainForm({ state }: HomeProps) {
       </div>
 
       <div className='formRow'>
-        <p>O próximo intervalo é de {state.config.workTime}min</p>
+        <p>O próximo intervalo é de XXmin</p>
       </div>
 
       <div className='formRow'>
