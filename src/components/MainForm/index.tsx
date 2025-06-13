74d78c5be52ab2a47ada5aeb0039322e0a7361ba
KarolSalvador <karolsalvador91@gmail.com>
2025-06-13 10:34:34 -0300

    input controlado no React com value e onChange

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,26 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('Deu certo!');
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='Task'
           id='MeuInput'
           type='text'
           placeholder='Ex.: estudar para a prova'
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
         />
       </div>
 
